a6bfd6a72ba0860fdd15e1944fce6e02c621b76c
J Mulder <jlmulder2014@gmail.com>
2020-06-07 20:53:34 -0700

    form-submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,19 @@
 // named in Js when that specific button is clicked on a specific alert pops up
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // callback function
 var createTaskHandler = function() {
+
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-  }
+   }
 
 //annonymous function
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
 
   
